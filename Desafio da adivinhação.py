d859fed9645114338a162f2889382db94bdadb46
Dara Fontoura <81888608+mdar4@users.noreply.github.com>
2021-06-15 16:36:48 -0300

    Update Desafio da adivinhação.py
    Desafio da Adivinhação

--- Desafio da adivinhação.py
+++ Desafio da adivinhação.py
@@ -1,10 +1,30 @@
-#Escreva um programa que pede a senha do usuário, e só sai do 
-#looping quando digitarem a senha correta
+# Desafio: Crie um jogo onde o computador vai “pensar” em um número entre 0 e 10. O jogador vai tentar adivinhar qual número foi
+# escolhido até acertar, entre os palpites diga ao jogador se o número do computador é maior ou menor ao que ele digitou,
+# no final mostre quantos palpites foram necessários para vencer.
 
-senha = '123456'
-while senha != '123456':
-    senha = (input("Digite sua senha: "))
-    if senha == '123456':
-        print('Conectando...')
+import random
+
+print('''
+Tente vencer adivinhando qual número será escolhido.
+Faça a escolha de 0 a 10.
+''')
+
+n = random.randint(0,10)
+escolha = int(input("Escolha um número de 0 a 10: "))
+tentativa = 0
+
+while True:
+    if n == escolha:
+      print("Parabéns ! Você acertou.")
+      tentativa = tentativa + 1
+      break
     else:
-        print('Senha incorreta! Tente novamente.')
+      print("Não foi dessa vez... Tente novamente !")
+      tentativa = tentativa + 1
+      if n >= escolha:
+        print('O computador jogou um número maior.')
+      else:
+        print('O computador jogou um número menor.')
+      escolha = int(input("Escolha um número de 0 a 10: "))
+
+print(f'Você jogou o número {escolha} e teve {tentativa} tentativas.')
