124fe8de8193fc1df777b371ef61d958bdd296a3
mdar4 <darasilva2391@gmail.com>
2021-06-18 20:57:24 -0300

    Exercício 1 da aula 10 - Listas Compostas

--- Ex01_aula10.py
+++ Ex01_aula10.py
@@ -1,7 +1,6 @@
 # Crie uma lista composta que recebe 5 nomes e 5 idades de clientes, utilizando o for e o if, verifique quais clientes são maiores de idade e quais são menores de idade e mostre na tela a seguinte frase para cada um deles: 'Fulano é maior de idade' ou 'Fulana é menor de idade' e  também quantos são maiores e quantos são menores de idade.
 
-maior = 0
-menor = 0
+maior = menor = 0
 povoado = [['Dara', 20],['Vinicius',21],['Janete',34],['Clara',12],['Duda', 15]]
 for pessoa in povoado:
     if pessoa[1] >= 18:
