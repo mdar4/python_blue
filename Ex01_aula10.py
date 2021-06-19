8fc8521792571be2c777b296346918b1ddcd1cd9
mdar4 <darasilva2391@gmail.com>
2021-06-19 20:03:52 -0300

    Exercício 1- aula 10- Listas compostas

--- Ex01_aula10.py
+++ Ex01_aula10.py
@@ -1,12 +1,21 @@
-# Crie uma lista composta que recebe 5 nomes e 5 idades de clientes, utilizando o for e o if, verifique quais clientes são maiores de idade e quais são menores de idade e mostre na tela a seguinte frase para cada um deles: 'Fulano é maior de idade' ou 'Fulana é menor de idade' e  também quantos são maiores e quantos são menores de idade.
+# 1° Crie uma lista composta que recebe 5 nomes e 5 idades de clientes, utilizando o for 
+# 2° Com if, verifique quais clientes são maiores de idade e quais são menores de idade.
+# 3° mostre na tela a seguinte frase para cada um deles: 'Fulano é maior de idade' ou 'Fulana é menor de idade' 
+# 4° e  também quantos são maiores e quantos são menores de idade.
 
+povoado = list()
+pessoa = list()
 maior = menor = 0
-povoado = [['Dara', 20],['Vinicius',21],['Janete',34],['Clara',12],['Duda', 15]]
-for pessoa in povoado:
-    if pessoa[1] >= 18:
-        idade = print(f'{pessoa[0]} é maior de idade.')
-        maior += 1
+for c in range(5):
+    pessoa.append(str(input('Nome: ')))
+    pessoa.append(str(input('Idade: ')))
+    povoado.append(pessoa[:])
+    pessoa.clear()
+for i in povoado:
+    if i[1]>= 18:
+        print(f'{p[0]} é maior de idade.')
+        maior +=1
     else:
-        idade= print(f'{pessoa[0]} é menor de idade.')
-        menor +=1
-print(f'{maior} pessoas são de maior.\n{menor} pessoas são de menor.')
+        print(f'{p[0]} é menor de idade.')
+        menor += 1
+print(f'{maior} são de maior e {menor} são de menor.')
